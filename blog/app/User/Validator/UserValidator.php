c6bd98bc42fe84d463d164d30774642f7a245246
xgx <326234556@qq.com>
2019-04-07 12:45:45 +0800

    架构8

--- blog/app/User/Validator/UserValidator.php
+++ blog/app/User/Validator/UserValidator.php
@@ -10,17 +10,19 @@ class UserValidator extends LaravelValidator
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
             'name' => 'required',
-            'age'  => 'integer',
-            'sex'=> 'integer'
+            'age'  => 'required|integer',
+            'sex'=> 'required|integer'
         ],
         ValidatorInterface::RULE_UPDATE => [
             'name' => 'required',
-            'age'  => 'integer',
-            'sex'=> 'integer'
+            'age'  => 'required|integer',
+            'sex'=> 'required|integer'
         ]
     ];
     protected $messages = [
         'name.required' => '名字不能为空',
+        'age.required' => '年龄不能为空',
+        'sex.required' => '性别不能为空',
         'age.integer' => '年龄只能为数字',
         'sex.integer' => '性别只能为数字',
     ];
