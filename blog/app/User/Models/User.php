6599f18534aa419753a70a4702ef7680e12cb006
xgx <326234556@qq.com>
2019-04-08 01:37:20 +0800

    jwt

--- blog/app/User/Models/User.php
+++ blog/app/User/Models/User.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\User\Models;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,71 +8,10 @@ use Illuminate\Database\Eloquent\Model;
  * Class User
  * @package App\User\Models
  */
-class User extends Model implements Authenticatable
+class User extends Model
 {
     public $timestamps = true;  // 自动维护创建时间
     protected $dateFormat = 'U'; // 时间储存格式
     protected $table = 'user'; // 数据库表名
     protected $fillable = ['name', 'age', 'sex','status','password']; // 数据库表字段
-
-    /**
-     * Get the name of the unique identifier for the user.
-     *
-     * @return string
-     */
-    public function getAuthIdentifierName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
-    public function getAuthIdentifier()
-    {
-        // TODO: Implement getAuthIdentifier() method.
-    }
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Get the token value for the "remember me" session.
-     *
-     * @return string
-     */
-    public function getRememberToken()
-    {
-        // TODO: Implement getRememberToken() method.
-    }
-
-    /**
-     * Set the token value for the "remember me" session.
-     *
-     * @param  string $value
-     * @return void
-     */
-    public function setRememberToken($value)
-    {
-        // TODO: Implement setRememberToken() method.
-    }
-
-    /**
-     * Get the column name for the "remember me" token.
-     *
-     * @return string
-     */
-    public function getRememberTokenName()
-    {
-        // TODO: Implement getRememberTokenName() method.
-    }
 }
